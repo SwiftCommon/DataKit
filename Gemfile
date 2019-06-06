bd16b08391123a1f062ce921260c57d315ecd604
nxtstep <code@supersimple.io>
2019-06-06 22:41:39 +0200

    Update Danger to version 6.0.9

--- Gemfile
+++ Gemfile
@@ -2,7 +2,7 @@ ruby "2.6.0"
 source "https://rubygems.org"
 
 gem "fastlane", "~>2.114"
-gem "danger", "~>5.11"
+gem "danger", "~>6.0"
 gem "danger-swiftlint", "~>0.18"
 gem "jazzy", "~>0.9"
 gem "danger-lgtm"
--- danger_plugins/protect_files.rb
+++ danger_plugins/protect_files.rb
@@ -3,6 +3,7 @@ module Danger
     def protect_files(path: nil, message: nil, fail_build: false, callback: nil)
       raise "You have to provide a message" if message.to_s.empty?
       raise "You have to provide a path" if path.to_s.empty?
+      raise "You have to provide a callback" if callback.nil?
 
       broken_rule = git.modified_files.include?(path)
 
